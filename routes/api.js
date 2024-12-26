f3e5038c0ef27bf86e08b0b6a7bdf6b290901d0c
ayushpdx05 <ayush05px@gmail.com>
2024-12-26 11:32:43 -0800

    disabled checkmark for enable image classific

--- routes/api.js
+++ routes/api.js
@@ -1086,31 +1086,40 @@ api.post('/updateLabels', async (req, res) => {
 
 
 api.post('/updateProject', async (req, res) => {
-	console.log("updateProject");
-
-	console.log(req.body);
-
-	// get url
-	var PName = req.body.PName,
-		admin = req.body.Admin,
-		IDX = parseInt(req.body.IDX),
-		user = req.cookies.Username;
-
-	// get form input
-	var new_project_name = req.body.project_name,
-		project_description = req.body.project_description;
-
-	var public_path = __dirname.replace('routes',''),
-		main_path = public_path + 'public/projects/';
-		
-	var auto_save = 1;
+	var admin;
+	admin = req.body.Admin;
+	console.log('admin is ',admin);
+    const { PName, IDX, project_name, project_description } = req.body;
 	
-	await db.runAsync("UPDATE Projects SET PDescription = '"+project_description+
-		"', AutoSave = '"+auto_save+
-		"' WHERE PName = '" + PName + "' AND Admin = '" + admin + "'");
+    console.log('Old Project Name:', PName);
+    console.log('New Project Name:', project_name);
+    console.log('New Project Description:', project_description);
+    
+    var public_path = __dirname.replace('routes',''),
+	main_path = public_path + 'public/projects/',
+	project_path = main_path + admin + '-' + PName;
 	
-	return res.redirect('/config/projSettings?IDX='+IDX);
+	console.log("Database path:", project_path+'/'+PName+'.db');
+	
+	var db = new sqlite3.Database(project_path+'/'+PName+'.db', (err) => {
+		console.log(db)
+		if (err) {
+			return console.error(err.message);
+		}
+		console.log('Connected to db.');
+	});
+
+	const sql = `UPDATE Projects SET PName = ? WHERE PName = ? AND Admin = ?`;
+    db.run(sql, [project_name, PName,admin], function (err) {
+        if (err) {
+            console.error("Error running SQL query:", err.message);
+            return res.status(500).send("SQL query error");
+        }
+        console.log(`Row(s) updated: ${this.changes}`);
+        res.status(200).send(`Row(s) updated: ${this.changes}`);
+    }); 
 });
+
 
 
 
