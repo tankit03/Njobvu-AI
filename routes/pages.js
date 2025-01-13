89f794543d41ea555e7826619340e8653555860b
ayushpdx05 <ayush05px@gmail.com>
2025-01-13 13:04:42 -0800

    download icon - ayush

--- routes/pages.js
+++ routes/pages.js
@@ -628,19 +628,47 @@ module.exports = {
 			PNames: PNames
         });
     },
-
+	
 	getAnnotatePage: async (req, res) => {
 		var username = req.cookies.Username;
 		console.log('Received IDX:', req.query.IDX);
 		var IDX = parseInt(req.query.IDX, 10);
 		
 		console.log('Parsed IDX:', IDX);
+		// get URL variables
+		var IDX = parseInt(req.query.IDX),
+			user = req.cookies.Username;
+
+		if(IDX == undefined)
+		{
+			IDX = 0;
+			valid = 1;
+			return res.redirect('/home');
+		}
+		if(user == undefined)
+		{
+			return res.redirect('/');
+		}
+
+		var projects = await db.allAsync("SELECT * FROM Access WHERE Username = '"+user+"'");
+
+		var num = IDX;
+
+		if(num >= projects.length){
+			valid = 1;
+			return res.redirect('/home');
+		}
+		var PName = projects[num].PName;
+		var admin = projects[num].Admin;
 		
+		var public_path = __dirname.replace('routes','');
+		var main_path = public_path + 'public/projects/';
+		var path = main_path + admin + '-' + PName + '/' + PName + '.db';	
 		if (isNaN(IDX)) {
 			console.error('Invalid IDX:', IDX);
 			return res.redirect('/home');
 		}
-	
+		
 		var projects = await db.allAsync("SELECT * FROM Access WHERE Username = '" + username + "'");
 		var project = projects[IDX];
 	
@@ -666,7 +694,12 @@ module.exports = {
 			}
 			console.log('Connected to pdb.');
 		});
-
+		var sdb = new sqlite3.Database(path, (err) => {
+			if (err) {
+				return console.error(err.message);
+			}
+			console.log('Connected to tdb.');
+		});
 		pdb.allAsync = function (sql) {
 			var that = this;
 			return new Promise(function (resolve, reject) {
@@ -683,11 +716,79 @@ module.exports = {
 				console.log(err)
 			});
 		};	
-
+		// create async database object functions
+		sdb.getAsync = function (sql) {
+			var that = this;
+			return new Promise(function (resolve, reject) {
+				that.get(sql, function (err, row) {
+					if (err)
+					{
+						console.log("runAsync ERROR! ", err);
+						reject(err);
+					}
+					else
+						resolve(row);
+				});
+			}).catch(err => {
+				console.log(err)
+			});
+		};
+		sdb.allAsync = function (sql) {
+			var that = this;
+			return new Promise(function (resolve, reject) {
+				that.all(sql, function (err, row) {
+					if (err)
+					{
+						console.log("runAsync ERROR! ", err);
+						reject(err);
+					}
+					else
+						resolve(row);
+				});
+			}).catch(err => {
+				console.log(err)
+			});
+		};		
 		var Classes = await pdb.allAsync("SELECT * FROM `Classes`");
+		var results1 = await db.getAsync("SELECT * FROM `Projects` WHERE PName = '" + PName + "' AND Admin = '" + admin + "'");
+		var results2 = await sdb.allAsync("SELECT * FROM `Classes`");
+		
+		var classes = [];
+		var counts = [];
+		var icounts = [];
+		var lcounts = 0;
+		for(var i = 0; i < results2.length; i++)
+		{
+			var results3 = await sdb.getAsync("SELECT COUNT(*) FROM Labels Where CName = '" + results2[i].CName + "'");
+			classes.push(results2[i].CName);
+			counts.push(results3["COUNT(*)"]);
+			var results4 = await sdb.allAsync("SELECT DISTINCT IName FROM Labels WHERE CName = '" + results2[i].CName + "'");
+			icounts.push(results4.length);
+		}
+		
+		var acc = await db.allAsync("SELECT * FROM `Access` WHERE PName = '" + PName + "' AND Admin = '" + admin + "'");
+        var access = [];
+		for(var i = 0; i < acc.length; i++)
+        {
+            access.push(acc[i].Username);
+		}
 
-		console.log('Classes:', Classes);
-			
+
+		var results5 = await sdb.getAsync("SELECT COUNT(*) FROM Images");
+		var results6 = await sdb.allAsync("SELECT DISTINCT IName FROM Labels");
+		var complete = Math.trunc(100*(results6.length/results5['COUNT(*)']));
+
+		// close the database
+		sdb.close(function(err){
+			if(err)
+			{
+				console.error(err);
+			}
+			else{
+				console.log("sdb closed successfully");
+			}
+		});
+		console.log(icounts,counts)
 		res.render('annotate', {
 			title: 'annotate',
 			user: username,
@@ -696,7 +797,10 @@ module.exports = {
 			PName: PName,
 			classes: Classes,
 			IDX: IDX,
-			activePage: 'Annotate' 
+			activePage: 'Annotate',
+			icounts:icounts,
+			
+
 
 		});
 	},
