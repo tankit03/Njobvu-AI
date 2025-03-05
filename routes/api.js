86b6a89755db5e55ad9ccc2a3a7fe2df9dd74135
Tanish Hupare <huparetanish@gmail.com>
2025-03-05 12:31:14 -0800

    Delete button working to review page - Tanish Hupare

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,8 @@ const { stdout } = require('process');
 const sharp = require('sharp');
 const api = express.Router();
 
+
+
 api.post('/logout', async (req, res) => {
 	res.clearCookie("Username");
 	res.redirect("/");
@@ -225,9 +227,6 @@ api.post('/signup', async (req, res) => {
 	
 	return res.redirect('/');
 });
-
-
-
 
 api.post('/createP', async (req, res) => {
 
@@ -6163,6 +6162,69 @@ api.post('/changeValidation', async(req, res) => {
 		res.send({"Success": "No"});
 	}
 });
+
+api.put('', async (req, res) => {
+
+	
+
+});
+
+api.delete('/deleteBadLabels/:Admin/:PName/:Lid', async (req, res) => {
+    try {
+        const Admin = req.params.Admin;
+        const PName = req.params.PName;
+        const Lid = req.params.Lid;
+
+        console.log("Admin: ", Admin);	
+        console.log("PName: ", PName);
+        console.log("Lid: ", Lid);
+
+        const public_path = __dirname.replace('routes', ''),
+            main_path = public_path + 'public/projects/',
+            project_path = main_path + Admin + '-' + PName;
+
+        const dbPath = project_path + '/' + PName + '.db';
+
+		console.log("this is the dbPath: ", dbPath);
+
+        const db = new sqlite3.Database(dbPath, (err) => {
+            if (err) {
+				console.log("hello");
+                console.error(err.message);
+                return res.status(500).send('Database connection error');
+            }
+            console.log(`Connected to ${PName} database`);
+        });
+
+        db.run('DELETE FROM Labels WHERE LID = ?', [Lid], function (err) {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).send('Internal server error');
+            } 
+            if (this.changes === 0) {
+                return res.status(404).send('Product not found');
+            } 
+            
+            console.log(`Deleted entry with ID: ${Lid}`);
+            return res.status(200).json({ message: `This transaction was deleted for ID: ${Lid}` });
+        });
+
+        db.close((err) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                console.log("Database connection closed");
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        if (!res.headersSent) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+});
+
 
 
 api.post('/batch-change-class', async(req,res) =>{
